7d6393c2ac055535f87c7a8eef4073e77ba081fa
et-rnal <control@eternal.sbs>
2023-12-02 22:31:28 +0900

    edited README.md, added aa-genkernel optional hook, fleshed out inst.sh

--- README.md
+++ README.md
@@ -1,11 +1,53 @@
-#What is this? 
+# What is this? 
 A port of void linux' default efibootmgr hook to work with gentoo dist-kernel
+If you'd like to see more about their implimentation, please see [void's efibootmgr package](https://github.com/void-linux/void-packages/tree/d97b4abbe11ff5b08e1a2c852489bc36dd2f64c5/srcpkgs/efibootmgr)
+This implimentation supplants the requirement of a traditional bootloader. It will automatically add `efibootmgr` entries upon kernel installation/upgrade. 
+
+# Why does this exist?
+
+Primarily as a learning tool for myself. I actually use this setup on my personal machines. If you've stumbled across this, please ensure you know what these scripts are doing. Your mileage may vary. You have been warned. 
 
 # How do I configure it? 
+
+## Manually
 Place the appropriate hooks from `etc_hooks/` in `/etc/kernel/` under their respective directories
 Place `etc_hooks/default/efibootmgr-kernel-hook` in the target device as `/etc/default/efibootmgr-kernel-hook`
 This will account for kernel version numbers present in `/boot` which are appended to the initrd and vmlinuz files by default both with `genkernel` and `dracut`
 Check the `efibootmgr-kernel-hook` file and place your own arguments inside, including UUID's and target device.
 
-## Depencencies
-on gentoo, ensure use flags for `initramfs` and `crpytsetup` are enabled. 
+## `dracut` vs `genkernel` 
+One important consideration is whether or not the target system will be utilizing `dracut` or `genkernel`. Both of these have different arguments required for LUKS to work. 
+
+
+### dracut cmdline example 
+```rd.luks.uuid=MY_ENCRYPTED_ROOT_UUID rd.luks.name=MY_ENCRYPTED_ROOT_UUID=crypt_root root=/dev/mapper/crypt_root
+```
+This would be placed in `/etc/default/efibootmgr-kernel-hook` in the OPTIONS variable for machines using dracut. Please read why you'd want both "rd.luks.uuid" and "rd.luks.name" present [here](https://github.com/dracutdevs/dracut/issues/1566)
+
+### genkernel cmdline example 
+```crypt_root=UUID=MY_ENCRYPTED_ROOT_UUID root=/dev/mapper/root
+```
+This would be placed in `/etc/default/efibootmgr-kernel-hook` in the OPTIONS variable for machines using genkernel.
+
+Note: If you want to leverage genkernel's initramfs (which works better in my opinion), disable the initramfs use flag for gentoo-kernel and install genkernel. 
+
+After this, you can drop a 2 liner in `/etc/kernel/postinst.d/` containing the following lines:
+```#!/bin/sh
+genkernel initramfs --luks
+```
+This will cause gentoo-kernel to automatically leverage genkernel to produce the initramfs vice dracut. 
+
+## inst.sh
+This is a quick deployment script to be ran in a chroot. This was made to function inside of archiso (but can be used by any GNU/Linux system) with the following disk layout
+sdc2 being LUKS, with crypt_root being the name of it's unlocked part in /dev/mapper/
+
+At the top of "inst.sh", simply plug in the target device (i.e /dev/sdc) and your desired InitramfsScheme (dracut or genkernel) and it will take care for the rest, including disabling the initramfs use flag for gentoo-kernel
+
+sdc                                                               
+├─sdc1         /mnt/boot               
+├─sdc2                                 
+│ └─crypt_root /mnt                    
+└─sdc3                                 
+
+# Depencencies
+`gentoo-kernel` (disable the initramfs use flag if you want to use genkernel instead), `genkernel` (optional, default is dracut) `cryptsetup`, `efibootmgr`, `linux-firmware`
--- inst.sh
+++ inst.sh
@@ -1,15 +1,177 @@
-#!/bin/sh
+#!/bin/bash
+######################
+# FILL OUT THE BELOW #
 
-deploy(){
-echo "Deploying scripts"
-cp -rPv etc_hooks/kernel/ /etc/
-mkdir /etc/default
-cp -v etc_hooks/default/efibootmgr-kernel-hook /etc/default/
+#/dev/sdX
+TargetDevice=""
+InitramfsScheme=""
+#genkernel or dracut
+
+######################
+
+refresh_uuid(){
+block_data=$(blkid $TargetDevice* 2>/dev/null)
+get_home_crypto_uuid=$(blkid $TargetDevice* | grep -e $TargetDevice"3" -e $TargetDevice"p3" | cut -d '"' -f 2)
+get_root_crypto_uuid=$(blkid $TargetDevice*| grep -e $TargetDevice"2" -e $TargetDevice"p2" | cut -d '"' -f 2)
+get_home_plain_uuid=$(lsblk /dev/mapper/crypt_home --output UUID --noheadings 2>/dev/null)
+get_root_plain_uuid=$(lsblk /dev/mapper/crypt_root --output UUID --noheadings 2>/dev/null)
+dev_name=$(echo $TargetDevice | cut -d '/' -f 3)
+get_root_puuid=$(lsblk $TargetDevice* --output NAME,PARTUUID --raw --noheadings | grep -e $dev_name"2" -e $dev_name"p2"| cut -d ' ' -f 2 | uniq | xargs)
+get_home_puuid=$(lsblk $TargetDevice* --output NAME,PARTUUID --raw --noheadings| grep -e $dev_name"3" -e $dev_name"p3" | cut -d ' ' -f 2 | uniq | xargs)
+get_boot_part=$(lsblk $TargetDevice* --raw --noheadings --output NAME| grep -e $dev_name"1" -e $dev_name"p1" | uniq | xargs)
+get_boot_uuid=$(blkid $TargetDevice*| grep -e $TargetDevice"1" -e $TargetDevice"p1" | cut -d '"' -f 2)
+get_device_id=$(blkid /dev/disk/by-id/* --match-token UUID="$get_boot_uuid" |xargs | cut -d '/' -f 5 | cut -d ':' -f 1 | sed 's/-part[0-9]*//g')
 }
 
+
+config_check(){
+doas_check=$(which doas 2>/dev/null)
+
+
+if [ -z $TargetDevice ]; then 
+	echo "No target device set, please read the config file!" && exit
+else 
+	echo -n
+fi
+
+if [ "$InitramfsScheme" = "dracut" ] || [ "$InitramfsScheme" = "genkernel" ]; then
+	echo "InitramfsScheme is $InitramfsScheme"
+else 
+	echo "No valid \$InitramfsScheme set! Please view the script, it requires either \"genkernel\" or \"dracut\"" && exit
+fi
+
+if [ ! -b $TargetDevice ]; then
+	echo "Target device is not reachable, check your config file!" && exit
+else 
+	echo -n
+fi
+if [ -z "$doas_check" ]; then
+	echo "Set PrivEsc to sudo"
+	PrivEsc="sudo"
+else
+	echo "Set PrivEsc to doas"
+	PrivEsc="doas"
+fi
+
+}
+
+
+deploy(){
+
+
+
+if [ $InitramfsScheme == "dracut" ]; then
+echo -----------------------------
+echo "dracut Configuration"
+echo \>\> Directory: /etc/dracut.conf.d \(will be configured\)
+echo \>\> File: 10.conf \(will be added\)
+echo \>\> Content:
+echo -e "hostonly=\"yes\""
+echo -e "add_drivers+=\" vfat \""
+echo -e "add_dracutmodules+=\" crypt \"" 
+echo -----------------------------
+echo
+echo "This will automatically create the appropriate initramfs and vmlinuz files in /boot to include the desired modules."
+read -p "Do you wish to create this file? [y/N]: " yn 
+case $yn in
+  "y"|"Y") echo;;
+  "n"|"N") echo "Understood." && sleep 2  && efihook_strap;;
+esac
+mkdir /mnt/etc/dracut.conf.d
+echo -e "hostonly=\"yes\"" > /mnt/etc/dracut.conf.d/10.conf
+echo -e "add_drivers+=\" vfat \"" >> /mnt/etc/dracut.conf.d/10.conf
+echo -e "add_dracutmodules+=\" dm crypt \"" >> /mnt/etc/dracut.conf.d/10.conf
+
+echo "Completed!"
+elif [ $InitramfsScheme == "genkernel" ]; then
+echo -----------------------------
+echo "genkernel Configuration"
+echo \>\> File: /etc/kernel/postinst.d/aa-genkernel 
+echo \>\> File: /etc/portage/package.use/gentoo-kernel 
+echo \>\> Actions:
+echo "1.) Deploy aa-genkernel, a hook to automatically generate an initramfs on kernel install/upgrade"
+echo "2.) Deploy "sys-kernel/gentoo-kernel -initramfs", preventing the dist kernel from pulling in dracut"
+echo -----------------------------
+
+echo "This will automatically ensure genkernel creates the initramfs files required to boot, instead of utilizing dracut for this."
+read -p "Do you wish to create this file? [y/N]: " yn 
+case $yn in
+  "y"|"Y") echo;;
+  "n"|"N") echo "Understood." && sleep 2  && efihook_strap;;
+esac
+$PrivEsc cp -v etc_hooks/kernel/postinst.d/aa-genkernel /mnt/etc/kernel/postinst.d/
+echo "sys-kernel/gentoo-kernel -initramfs" > /mnt/etc/portage/package.use/gentoo-kernel
+echo "sys-kernel/gentoo-kernel -initramfs >> /mnt/etc/portage/package.use/gentoo-kernel"
+echo "Completed!"
+
+
+
+else 
+echo "Unknown initramfs option, how did you make it this far?"
+exit
+fi
+
+
+
+
+}
+efi_config(){
+if [ "$InitramfsScheme" == "genkernel" ]; then
+boot_args="crypt_root=UUID=$get_root_crypto_uuid root=/dev/mapper/root"
+quiet_settings="quiet loglevel=3"
+elif [ "$InitramfsScheme" == "dracut" ]; then
+boot_args="rd.luks.uuid=$get_root_crypto_uuid rd.luks.name=$get_root_crypto_uuid=crypt_root root=/dev/mapper/crypt_root"
+quiet_settings="quiet loglevel=3"
+echo $boot_args
+fi
+echo -----------------------------
+echo "efibootmgr Configuration (generated for: $InitramfsScheme)"
+echo \>\> Directory: /etc/default \(will be configured\)
+echo \>\> File: efibootmgr-kernel-hook \(will be added\)
+echo \>\> Content:
+echo "MODIFY_EFI_ENTRIES=1" 
+echo -e "OPTIONS=\"$quiet_settings fbcon=font:TER16x32 fbcon=nodefer $boot_args\"" 
+echo -e "DISK=\"/dev/disk/by-id/$get_device_id\"" 
+echo -e "PART=1" 
+echo -----------------------------
+echo "This step will configure efibootmgr to be automatically updated upon kernel installation/upgrade/removal."
+#echo "This is triggered by the two following files, which are executed automatically on kernel version changes"
+#echo "/etc/kernel/postinst.d/zz-efibootmgr"
+#echo "/etc/kernel/postrm.d/zz-efibootmgr"
+echo "This supplants the need for GRUB, or any similar piece of software"
+
+read -p "Do you understand and wish to continue? [y/N]: " yn 
+case $yn in
+  "y"|"Y") echo;;
+  "n"|"N") echo "Understood." && sleep 2  && exit;;
+esac
+mkdir /mnt/etc/default
+touch /mnt/etc/default/efibootmgr-kernel-hook
+echo "MODIFY_EFI_ENTRIES=1" > /mnt/etc/default/efibootmgr-kernel-hook
+echo -e "OPTIONS=\"loglevel=3 $quiet_settings fbcon=font:TER16x32 fbcon=nodefer $boot_args\"" >> /mnt/etc/default/efibootmgr-kernel-hook
+echo -e "DISK=\"/dev/disk/by-id/$get_device_id\"" >> /mnt/etc/default/efibootmgr-kernel-hook
+echo -e "PART=1" >> /mnt/etc/default/efibootmgr-kernel-hook
+
+sleep 2
+echo "----------"
+echo "Installing kernel postinst.d and postrm.d hooks."
+
+$PrivEsc mkdir /mnt/etc/kernel/postinst.d --parents --verbose
+$PrivEsc mkdir /mnt/etc/kernel/postrm.d --parents --verbose 
+$PrivEsc cp -v etc_hooks/kernel/postinst.d/zz-efibootmgr /mnt/etc/kernel/postinst.d/
+$PrivEsc cp -v etc_hooks/kernel/postrm.d/zz-efibootmgr /mnt/etc/kernel/postrm.d/
+echo "Hook installation finished!"
+
+}
+
+kernel_hook(){
+
+if [ "$InitramfsScheme" == "dracut" ]; then
 echo "Please ensure you have read the README document before proceeding."
 echo "This will deploy the following "
 echo "/etc/
+├── dracut.conf.d
+│   └── 10.conf
 ├── default
 │   └── efibootmgr-kernel-hook
 └── kernel
@@ -18,6 +180,20 @@ echo "/etc/
     └── postrm.d
         └── zz-efibootmgr
 "
+elif [ "$InitramfsScheme" == "genkernel" ]; then
+echo "Please ensure you have read the README document before proceeding."
+echo "This will deploy the following "
+echo "/etc/
+├── default
+│   └── efibootmgr-kernel-hook
+└── kernel
+    ├── postinst.d
+    │   └── zz-efibootmgr
+    │   └── aa-genkernel 
+    └── postrm.d
+        └── zz-efibootmgr
+"
+fi
 sleep 2 
 
 echo "Once the script has been deployed, ensure you check /etc/default/efibootmgr-kernel-hook and place your OWN values into it, then run emerge --config gentoo-kernel"
@@ -27,3 +203,12 @@ case $yn in
   "y"|"Y") deploy;;
   "n"|"N") echo "Understood." && sleep 2 ;;
 esac
+}
+
+
+refresh_uuid
+config_check
+refresh_uuid
+kernel_hook
+refresh_uuid
+efi_config
